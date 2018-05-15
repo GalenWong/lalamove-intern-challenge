187291f16fcd29d0d480ed37c977db1aedf380ba
GalenWong <wonggalen1999@gmail.com>
2018-05-14 22:05:02 -0700

    sorting now considers major versions

--- main.go
+++ main.go
@@ -1,46 +1,59 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"	// for reading command-line arguments
-	"bufio"	// for reading file by lines
-	"errors"// for error handling
-	"sort"	// for sorting
-	"github.com/coreos/go-semver/semver"
-	"github.com/google/go-github/github"
+        "context"
+        "fmt"
+        "os"    // for reading command-line arguments
+        "bufio" // for reading file by lines
+        "errors"// for error handling
+        "sort"  // for sorting
+        "github.com/coreos/go-semver/semver"
+        "github.com/google/go-github/github"
 )
 
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
-	var versionSlice []*semver.Version
+        var versionSlice []*semver.Version
+
 	if minVersion == nil {
-		return versionSlice
-	}
-	if len(releases) <= 0 {	// empty slice
-		return versionSlice
-	}
-	for _, release := range releases{	// adding all versions >= minVersion into versionSlice
-		if !(release.LessThan(*minVersion)) {
-			versionSlice = append(versionSlice, release)
-		}
-	}
+                return versionSlice
+        }
+        if len(releases) <= 0 { // empty slice
+                return versionSlice
+        }
+        for _, release := range releases{       // adding all versions >= minVersion into versionSlice
+                if !(release.LessThan(*minVersion)) {
+                        versionSlice = append(versionSlice, release)
+                }
+        }
 
-	DescendingSort(versionSlice)	// sorting verssionSlice into descending order
+        if len(versionSlice) == 0{
+                return versionSlice
+        }
 
-	var prevMaxMinor = versionSlice[0].Minor
-	var result []*semver.Version
+        DescendingSort(versionSlice)    // sorting verssionSlice into descending order
 
-	result = append(result, versionSlice[0])
+        var prevMaxMajor = versionSlice[0].Major
+        var prevMaxMinor = versionSlice[0].Minor
+        var result []*semver.Version
 
-	for _, release := range versionSlice{
-		if (release.Minor < prevMaxMinor) {
-			prevMaxMinor = release.Minor
-			result = append(result, release)
-		}
-	}
+        result = append(result, versionSlice[0])
 
-	return result
+        for _, release := range versionSlice{
+                if release.Major < prevMaxMajor {
+                        prevMaxMinor = release.Minor
+                        prevMaxMajor = release.Major
+                        result = append(result, release)
+                        continue
+                }
+                if release.Minor < prevMaxMinor {
+                        prevMaxMinor = release.Minor
+                        result = append(result, release)
+                }
+
+        }
+
+        return result
 }
 
 
@@ -48,67 +61,67 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 type Versions []*semver.Version
 
 func (s Versions) Len() int {
-	return len(s)
+        return len(s)
 }
 
 func (s Versions) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
+        s[i], s[j] = s[j], s[i]
 }
 
 func (s Versions) Less(i, j int) bool {
-	return s[i].LessThan(*s[j])
+        return s[i].LessThan(*s[j])
 }
 
 // Sort sorts the given slice of Version
 func DescendingSort(versions []*semver.Version) {
-	sort.Sort(sort.Reverse(Versions(versions)))
+        sort.Sort(sort.Reverse(Versions(versions)))
 }
 
 // separate a string into author, repo, minVer
 func ProcessString(str string) (author string, repo string, minVer *semver.Version, err error){
-	var i int
-	runes := []rune(str)
-	var tokenPos int
+        var i int
+        runes := []rune(str)
+        var tokenPos int
 
-	// default error value to be returned
-	author 	= ""
-	repo 	= ""
-	minVer 	= nil
-	err 	= errors.New(fmt.Sprintf("invalid string: \"%s\"", str))
+        // default error value to be returned
+        author  = ""
+        repo    = ""
+        minVer  = nil
+        err     = errors.New(fmt.Sprintf("invalid string: \"%s\"", str))
 
-	for i = 0; i < len(str); i++ {
-		if(str[i] == '/'){
-			author = string(runes[0:i])
-			tokenPos = i
-			break
-		}
-	}
+        for i = 0; i < len(str); i++ {
+                if(str[i] == '/'){
+                        author = string(runes[0:i])
+                        tokenPos = i
+                        break
+                }
+        }
 
-	if i == len(str) {
-		return // error value
-	}
+        if i == len(str) {
+                return // error value
+        }
 
-	for ; i < len(str); i++ {
-		if(str[i] == ','){
-			repo = string(runes[tokenPos+1:i])
-			tokenPos = i
-			break
-		} 
-	}
+        for ; i < len(str); i++ {
+                if(str[i] == ','){
+                        repo = string(runes[tokenPos+1:i])
+                        tokenPos = i
+                        break
+                } 
+        }
 
-	if i == len(str) {
-		return // error value
-	}
+        if i == len(str) {
+                return // error value
+        }
 
-	defer func(){
-		if recover() != nil {
-			// just return error value
-		}
-	}()
-	
-	minVer = semver.New(string(runes[tokenPos+1:]))
+        defer func(){
+                if recover() != nil {
+                        // just return error value
+                }
+        }()
+        
+        minVer = semver.New(string(runes[tokenPos+1:]))
 
-	return author, repo, minVer, nil
+        return author, repo, minVer, nil
 }
 
 // Here we implement the basics of communicating with github through the library as well as printing the version
@@ -116,48 +129,50 @@ func ProcessString(str string) (author string, repo string, minVer *semver.Versi
 // Please use the format defined by the fmt.Printf line at the bottom, as we will define a passing coding challenge as one that outputs
 // the correct information, including this line
 func main() {
-	pathToFile := os.Args[1]
+        pathToFile := os.Args[1]
 
-	file, err := os.Open(pathToFile) 
+        file, err := os.Open(pathToFile) 
 
-	if err != nil{
-		panic(err)
-	}
+        if err != nil{
+                panic(err)
+        }
 
-	defer file.Close()
+        defer file.Close()
 
-	// Github
-	client := github.NewClient(nil)
-	ctx := context.Background()
-	opt := &github.ListOptions{PerPage: 10}
+        // Github
+        client := github.NewClient(nil)
+        ctx := context.Background()
+        opt := &github.ListOptions{Page: 2, PerPage: 10}
 
-	scanner := bufio.NewScanner(file)
+        scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {	// for each line 
-		author, repo, minVer, err := ProcessString(scanner.Text());
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		releases, _, err := client.Repositories.ListReleases(ctx, author, repo, opt)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+        for scanner.Scan() {    // for each line 
+                author, repo, minVer, err := ProcessString(scanner.Text());
+                if err != nil {
+                        fmt.Println(err)
+                        continue
+                }
+                releases, _, err := client.Repositories.ListReleases(ctx, author, repo, opt)
+                if err != nil {
+                        fmt.Println(err)
+                        continue
+                }
 
-		allReleases := make([]*semver.Version, len(releases))
+                allReleases := make([]*semver.Version, len(releases))
 
 		for i, release := range releases {
-			versionString := *release.TagName
-			if versionString[0] == 'v' {
-				versionString = versionString[1:]
-			}
-			allReleases[i] = semver.New(versionString)
-		}
+                        versionString := *release.TagName
+                        if versionString[0] == 'v' {
+                                versionString = versionString[1:]
+                        }
+                        allReleases[i] = semver.New(versionString)
+                }
 
-		fmt.Println("here with string :", scanner.Text())
+                fmt.Println("releases: ", allReleases)
 
-		versionSlice := LatestVersions(allReleases, minVer)
-		fmt.Printf("latest versions of %s/%s: %s\n", author, repo, versionSlice)
-	}
+                versionSlice := LatestVersions(allReleases, minVer)
+                fmt.Printf("latest versions of %s/%s: %s\n", author, repo, versionSlice)
+        }
+
+        file.Close()
 }
